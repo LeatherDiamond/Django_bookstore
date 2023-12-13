c65735f4f009fcc42ccda621566edf6c7761a7b8
Aliaksandr Dauhuchyts <alexander.dovguchits@gmail.com>
2023-12-13 11:12:36 +0100

    flake8 applied to proj

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from catalog.views import SearchResultView
+
 from django.conf import settings
 from django.conf.urls.static import static
-from catalog.views import SearchResultView
+from django.contrib import admin
+from django.urls import include, path
+
 
 urlpatterns = [
     path("s-admin/", admin.site.urls),
