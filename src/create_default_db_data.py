efe64b0d9fd8d5b0696c21bcfa1db39ad1e88878
Aliaksandr Dauhuchyts <alexander.dovguchits@gmail.com>
2023-12-14 13:47:55 +0100

    project dockerized

---
+++ src/create_default_db_data.py
@@ -0,0 +1,29 @@
+import contextlib
+
+import django
+
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "proj.settings"
+from django.conf import settings
+
+load_dotenv(find_dotenv())
+
+if not settings.configured:
+    django.setup()
+
+from catalog.models import AppUser
+
+
+def create_superuser_for_adminpanel():
+    AppUser.objects.create_superuser(
+        username=os.getenv("DJANGO_SUPERUSER_USERNAME"),
+        password=os.getenv("DJANGO_SUPERUSER_PASSWORD"),
+    )
+
+
+if __name__ == "__main__":
+    with contextlib.suppress(django.db.utils.IntegrityError):
+        create_superuser_for_adminpanel()
